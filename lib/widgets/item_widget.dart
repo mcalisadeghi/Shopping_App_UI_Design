d3610ddf34973317ffe1abdf52775520ea039de5
mcalisadeghi <ali.ke970@gmail.com>
2023-08-31 20:25:15 +0330

    add item card

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -8,92 +8,129 @@ class ItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView.count(
       childAspectRatio: 0.68,
+      physics: const NeverScrollableScrollPhysics(),
       shrinkWrap: true,
       crossAxisCount: 2,
       children: [
-        Container(
-          padding: const EdgeInsets.only(
-            left: 15,
-            right: 15,
-            top: 10,
-          ),
-          margin: const EdgeInsets.symmetric(
-            vertical: 8,
-            horizontal: 10,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(
-              20,
+        for (int i = 1; i < 8; i++)
+          Container(
+            padding: const EdgeInsets.only(
+              left: 15,
+              right: 15,
+              top: 10,
             ),
-          ),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(
-                      5,
+            margin: const EdgeInsets.symmetric(
+              vertical: 8,
+              horizontal: 10,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(
+                20,
+              ),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(
+                        5,
+                      ),
+                      decoration: BoxDecoration(
+                        color: const Color(
+                          0xFF4C53A5,
+                        ),
+                        borderRadius: BorderRadius.circular(
+                          20,
+                        ),
+                      ),
+                      child: const Text(
+                        '50%',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                    decoration: BoxDecoration(
-                      color: const Color(
+                    const Icon(
+                      Icons.favorite_border,
+                      color: Colors.red,
+                    ),
+                  ],
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: const EdgeInsets.all(
+                      10,
+                    ),
+                    child: Image.asset(
+                      'assets/images/$i.png',
+                      height: 120,
+                      width: 120,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(
+                    bottom: 8,
+                  ),
+                  alignment: Alignment.centerLeft,
+                  child: const Text(
+                    'Product Title',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color(
                         0xFF4C53A5,
                       ),
-                      borderRadius: BorderRadius.circular(
-                        20,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: const Text(
+                    'write description of product',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(
+                        0xFF4C53A5,
                       ),
                     ),
-                    child: const Text(
-                      '50%',
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 10,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '\$55',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(
+                            0xFF4C53A5,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  const Icon(
-                    Icons.favorite_border,
-                    color: Colors.red,
-                  ),
-                ],
-              ),
-              InkWell(
-                onTap: () {},
-                child: Container(
-                  margin: const EdgeInsets.all(
-                    10,
-                  ),
-                  child: Image.asset(
-                    'assets/images/1.png',
-                    height: 120,
-                    width: 120,
+                      Icon(
+                        Icons.shopping_cart_checkout,
+                        color: Color(
+                          0xFF4C53A5,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.only(
-                  bottom: 8,
-                ),
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Product Title',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Color(
-                      0xFF4C53A5,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
       ],
     );
   }
