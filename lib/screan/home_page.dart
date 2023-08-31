9ba20f8ea7ba05f2f4ab1d6a39eee51cee2baf9d
mcalisadeghi <ali.ke970@gmail.com>
2023-08-31 16:45:03 +0330

    search bar

--- lib/screan/home_page.dart
+++ lib/screan/home_page.dart
@@ -18,8 +18,89 @@ class _ShoppingAppState extends State<ShoppingApp> {
       //   ],
       // )
       body: ListView(
-        children: const <Widget>[
-          HomePageAppBar(),
+        children: <Widget>[
+          const HomePageAppBar(),
+          Container(
+            height: 500,
+            padding: const EdgeInsets.only(
+              top: 15,
+            ),
+            decoration: const BoxDecoration(
+              color: Color(
+                0xFFEDECF2,
+              ),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(
+                  35,
+                ),
+                topRight: Radius.circular(
+                  35,
+                ),
+              ),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                  ),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(
+                      30,
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(
+                          left: 5,
+                        ),
+                        height: 50,
+                        width: 300,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Search here...',
+                          ),
+                        ),
+                      ),
+                      const Spacer(),
+                      const Icon(
+                        Icons.camera_alt,
+                        size: 27,
+                        color: Color(
+                          0xFF4C53A5,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 10,
+                  ),
+                  child: Text(
+                    'Categories',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Color(
+                        0xFF4C53A5,
+                      ),
+                    ),
+                  ),
+                ),
+                //categories
+              ],
+            ),
+          ),
         ],
       ),
     );
