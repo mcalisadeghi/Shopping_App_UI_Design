1d1de4ad100f47a1aa1b3dcb29736e16980cdd68
mcalisadeghi <ali.ke970@gmail.com>
2023-08-31 22:58:26 +0330

    card Item samples

--- lib/pagages/cartPage.dart
+++ lib/pagages/cartPage.dart
@@ -1,6 +1,7 @@
 // Import necessary packages
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pagages/CartAppBar.dart';
+import 'package:flutter_application_1/widgets/CartItemSamples.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -14,8 +15,32 @@ class _CartPageState extends State<CartPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: const [
-          CartAppBar(),
+        children: <Widget>[
+          const CartAppBar(),
+          Container(
+            height: 700,
+            padding: const EdgeInsets.only(
+              top: 15,
+            ),
+            decoration: const BoxDecoration(
+              color: Color(
+                0xFFEDECF2,
+              ),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(
+                  35,
+                ),
+                topRight: Radius.circular(
+                  35,
+                ),
+              ),
+            ),
+            child: const Column(
+              children: <Widget>[
+                CartItemSamples(),
+              ],
+            ),
+          )
         ],
       ),
     );
---
+++ lib/widgets/CartItemSamples.dart
@@ -0,0 +1,173 @@
+// Import necessary packages
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CartItemSamples extends StatefulWidget {
+  const CartItemSamples({super.key});
+
+  @override
+  State<CartItemSamples> createState() => _CartItemSamplesState();
+}
+
+class _CartItemSamplesState extends State<CartItemSamples> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        for (int i = 1; i < 4; i++)
+          Container(
+            height: 110,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 10,
+            ),
+            padding: const EdgeInsets.all(
+              10,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(
+                10,
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                Radio(
+                  value: "",
+                  activeColor: const Color(
+                    0xFF4C53A5,
+                  ),
+                  groupValue: "",
+                  onChanged: (index) {},
+                ),
+                Container(
+                  height: 70,
+                  width: 70,
+                  margin: const EdgeInsets.only(
+                    right: 35,
+                  ),
+                  child: Image.asset(
+                    'assets/images/$i.png',
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 10,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Product Title',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Color(
+                            0xFF4C53A5,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '\$55',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(
+                            0xFF4C53A5,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 5,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      const Icon(
+                        Icons.delete,
+                        color: Colors.red,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(
+                              5,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(
+                                20,
+                              ),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(
+                                    0.5,
+                                  ),
+                                  spreadRadius: 1,
+                                  blurRadius: 10,
+                                ),
+                              ],
+                            ),
+                            child: const Icon(
+                              CupertinoIcons.plus,
+                              size: 18,
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.symmetric(
+                              vertical: 10,
+                            ),
+                            child: const Text(
+                              '01',
+                              style: TextStyle(
+                                color: Color(
+                                  0xFF4C53A5,
+                                ),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(
+                              5,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(
+                                20,
+                              ),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(
+                                    0.5,
+                                  ),
+                                  spreadRadius: 1,
+                                  blurRadius: 10,
+                                ),
+                              ],
+                            ),
+                            child: const Icon(
+                              CupertinoIcons.minus,
+                              size: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+      ],
+    );
+  }
+}
