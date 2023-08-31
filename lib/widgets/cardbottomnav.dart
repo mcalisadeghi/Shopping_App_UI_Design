252e429f16e93cb7a1a80017e5865223725698af
mcalisadeghi <ali.ke970@gmail.com>
2023-09-01 02:29:35 +0330

    cardbouttom_nav

--- lib/widgets/cardbottomnav.dart
+++ lib/widgets/cardbottomnav.dart
@@ -11,7 +11,64 @@ class _CardBottomNavState extends State<CardBottomNav> {
   @override
   Widget build(BuildContext context) {
     return BottomAppBar(
-      child: Container(),
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 15,
+        ),
+        height: 130,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'total:',
+                  style: TextStyle(
+                    color: Color(
+                      0xFF4C53A5,
+                    ),
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '\$250',
+                  style: TextStyle(
+                    color: Color(
+                      0xFF4C53A5,
+                    ),
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: const Color(
+                  0xC53A5,
+                ),
+                borderRadius: BorderRadius.circular(
+                  20,
+                ),
+              ),
+              child: const Text(
+                'chek out',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
