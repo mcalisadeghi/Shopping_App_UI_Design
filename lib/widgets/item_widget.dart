b1334ddf76a11883ae85556271069a5e7e9e6c8f
mcalisadeghi <ali.ke970@gmail.com>
2023-08-31 18:07:16 +0330

    add card items

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -44,8 +44,52 @@ class ItemWidget extends StatelessWidget {
                         20,
                       ),
                     ),
+                    child: const Text(
+                      '50%',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const Icon(
+                    Icons.favorite_border,
+                    color: Colors.red,
                   ),
                 ],
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  margin: const EdgeInsets.all(
+                    10,
+                  ),
+                  child: Image.asset(
+                    'assets/images/1.png',
+                    height: 120,
+                    width: 120,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                  bottom: 8,
+                ),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Product Title',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Color(
+                      0xFF4C53A5,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
               ),
             ],
           ),
