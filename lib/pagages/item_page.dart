d0c6bb00b0291edb01a2decdb77cc6ae3a51c65c
mcalisadeghi <ali.ke970@gmail.com>
2023-09-01 21:44:02 +0330

    item page add pic favorite and heart

--- lib/pagages/item_page.dart
+++ lib/pagages/item_page.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'package:clippy_flutter/clippy_flutter.dart';
 import 'package:flutter_application_1/widgets/item_appbar.dart';
+
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class ItemPages extends StatefulWidget {
   const ItemPages({super.key});
@@ -36,13 +38,13 @@ class _ItemPagesState extends State<ItemPages> {
             child: Container(
               width: double.infinity,
               color: Colors.white,
-              child: const Padding(
-                padding: EdgeInsets.symmetric(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
                   horizontal: 20,
                 ),
                 child: Column(
                   children: [
-                    Padding(
+                    const Padding(
                       padding: EdgeInsets.only(
                         top: 50,
                         bottom: 20,
@@ -53,7 +55,113 @@ class _ItemPagesState extends State<ItemPages> {
                             'Product Title',
                             style: TextStyle(
                               fontSize: 28,
+                              color: Color(
+                                0xFF4C53A5,
+                              ),
+                              fontWeight: FontWeight.bold,
                             ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 4,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          RatingBar.builder(
+                            initialRating: 4,
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            itemCount: 5,
+                            itemSize: 20,
+                            itemPadding: const EdgeInsets.symmetric(
+                              horizontal: 4,
+                            ),
+                            itemBuilder: (contex, _) => const Icon(
+                              Icons.favorite,
+                              color: Color(
+                                0xFF4C53A5,
+                              ),
+                            ),
+                            onRatingUpdate: (index) {},
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(
+                                  5,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(
+                                    20,
+                                  ),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(
+                                        0.5,
+                                      ),
+                                      spreadRadius: 3,
+                                      blurRadius: 3,
+                                      offset: const Offset(
+                                        0,
+                                        3,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                child: const Icon(
+                                  CupertinoIcons.minus,
+                                  size: 18,
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.symmetric(
+                                  horizontal: 10,
+                                ),
+                                child: const Text(
+                                  '01',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Color(
+                                      0xFF4C53A5,
+                                    ),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(
+                                  5,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(
+                                    20,
+                                  ),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(
+                                        0.5,
+                                      ),
+                                      spreadRadius: 3,
+                                      blurRadius: 3,
+                                      offset: const Offset(
+                                        0,
+                                        3,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                child: const Icon(
+                                  CupertinoIcons.plus,
+                                  size: 18,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
