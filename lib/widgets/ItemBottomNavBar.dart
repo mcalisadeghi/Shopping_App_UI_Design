457d2809943a3b249fbbd0925393c843d6dcba09
mcalisadeghi <ali.ke970@gmail.com>
2023-09-01 22:42:11 +0330

    build ItemBottomNavBar

---
+++ lib/widgets/ItemBottomNavBar.dart
@@ -0,0 +1,11 @@
+// Import necessary packages
+import 'package:flutter/material.dart';
+
+class ItemBottomNavBar extends StatelessWidget {
+  const ItemBottomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
